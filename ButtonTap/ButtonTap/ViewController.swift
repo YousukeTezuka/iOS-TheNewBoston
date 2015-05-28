4a95fa12d79f7bb2a9c2bc1e9f94b66c7940c4ba
YousukeTezuka <YousukeTezuka@gmail.com>
2015-05-28 18:00:49 +0900

    Finished Chapter9

--- ButtonTap/ButtonTap/ViewController.swift
+++ ButtonTap/ButtonTap/ViewController.swift
@@ -1,9 +1,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myLabel: UILabel!
 
     @IBAction func buttonPressed(sender: UIButton) {
-        
+        let title = sender.titleForState(.Normal)!
+        myLabel.text = "You clicked the \(title) button"
     }
     
 }
